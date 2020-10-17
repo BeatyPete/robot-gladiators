872d487888e9ef923d0f72d92e59edfd9aee66ff
BeatyPete <akira941@gmail.com>
2020-10-17 01:24:16 -0500

    random attack and health

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,6 +10,13 @@ var enemyNames = ["Robaato", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd liked to fight or run
@@ -24,14 +31,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -50,7 +58,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -64,7 +73,7 @@ var fight = function(enemyName) {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
       }
     }
-  };
+};
 
 var startGame = function() {
   for(var i = 0; i < enemyNames.length; i++) {
@@ -78,7 +87,7 @@ var startGame = function() {
         //pick enemt
         var pickedEnemyName = enemyNames[i];
         //reset enemy health
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60)
         //picked enemy name becomes enemy name 
         fight(pickedEnemyName);
         //if not at last enemy and player still alive
@@ -113,16 +122,17 @@ var endGame = function() {
     window.alert("You've lost your robot in battle.");
   }
 
-  // ask player if they'd like to play again
-var playAgainConfirm = window.confirm("Would you like to play again?");
+    // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");{
 
-if (playAgainConfirm) {
-  // restart the game
-  startGame();
-} 
-else {
-  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-}
+    if (playAgainConfirm) {
+      // restart the game
+      startGame();
+    } 
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+  }
 };
 
 var shop = function() {
